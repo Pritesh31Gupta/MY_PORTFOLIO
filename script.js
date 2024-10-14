9a4d0614c7a302befcd0ae35b3c6640a8105da59
Pritesh31Gupta <pritesh31kumar@gmail.com>
2024-10-14 13:12:12 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+// For future interactivity and JavaScript features
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio website is ready!");
+  });
+  const submit = document.querySelector('su');
+  su.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    
+  })
+  submit.innerHTML=`your respose has been recorded`;
